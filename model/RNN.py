e3566354ac0a5a4ae38ac430d0e19f7f64db6187
unes <89698370+regyonu@users.noreply.github.com>
2026-05-23 04:38:29 +0100

    Update RNN.py

--- model/RNN.py
+++ model/RNN.py
@@ -56,8 +56,8 @@ class Model(nn.Module):
         # RNN forward
         out, _ = self.rnn(x)       # [B, T, d_model]
 
-        # Temporal mean pooling
-        out = out.mean(dim=1)       # [B, d_model]
+        
+        out = out[:, -1, :]     # [B, d_model]
 
         # Forecast projection
         out = self.projection(out)
